eecd3584702f5698e935fc636f3a668000fee9f2
Michiel Vancoillie <info@ninetynine.be>
2012-12-02 02:06:35 +0100

    Next screen selected

--- src/com/flatturtle/myturtlecontroller/MainActivity.java
+++ src/com/flatturtle/myturtlecontroller/MainActivity.java
@@ -38,8 +38,13 @@ import com.flatturtle.myturtlecontroller.ui.MainButton;
 public class MainActivity extends Activity {
 	private PendingIntent intent;
 	private LinearLayout containerButtons;
+	private View viewStart;
+	private View viewLocation;
+	private TextView lblMode;
+	private RelativeLayout btnBack;
+	private RelativeLayout btnGo;
 	private ArrayList<RelativeLayout> listButtons;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -63,26 +68,33 @@ public class MainActivity extends Activity {
 			}
 		});
 
+		// Link some views
 		containerButtons = (LinearLayout) findViewById(R.id.containerButtons);
-
-		// Buttons
+		viewStart = findViewById(R.id.viewStart);
+		viewLocation = findViewById(R.id.viewLocation);
+		viewLocation.setVisibility(View.INVISIBLE);
+		lblMode = (TextView) findViewById(R.id.lblMode);
+		btnBack = (RelativeLayout) findViewById(R.id.btnBack);
+		btnGo = (RelativeLayout) findViewById(R.id.btnGo);
+		
+		// Create buttons
 		ArrayList<MainButton> btns = new ArrayList<MainButton>();
 		
 		ImageView icon = new ImageView(this);
 		icon.setImageResource(R.drawable.icon_walk);
-		btns.add(new MainButton(icon, "btnWalk", "Walk", 1));
+		btns.add(new MainButton(icon, getString(R.string.otp_mode_walk), getString(R.string.btn_walk), 1));
 		
 		icon = new ImageView(this);
 		icon.setImageResource(R.drawable.icon_bike);
-		btns.add(new MainButton(icon, "btnBike", "Bike", 1));
+		btns.add(new MainButton(icon, getString(R.string.otp_mode_bike), getString(R.string.btn_bike), 1));
 		
 		icon = new ImageView(this);
 		icon.setImageResource(R.drawable.icon_public);
-		btns.add(new MainButton(icon, "btnPublic", "Public\ntransport", 2));
+		btns.add(new MainButton(icon, getString(R.string.otp_mode_transit), getString(R.string.btn_public_transport), 2));
 
 		icon = new ImageView(this);
 		icon.setImageResource(R.drawable.icon_car);
-		btns.add(new MainButton(icon, "btnCar", "Car", 1));
+		btns.add(new MainButton(icon, getString(R.string.otp_mode_car), getString(R.string.btn_car), 1));
 		
 		int count = 0;
 		for (MainButton btn : btns) {
@@ -113,6 +125,7 @@ public class MainActivity extends Activity {
 			label.setTextColor(Color.parseColor("#ffffff"));
 			label.setLines(btn.lines);
 			label.setGravity(Gravity.CENTER);
+			label.setWidth(200);
 			newButton.addView(label);
 			newButton.addView(btn.icon);
 			
@@ -129,10 +142,13 @@ public class MainActivity extends Activity {
 			lp.addRule(RelativeLayout.CENTER_VERTICAL);
 			btn.icon.setLayoutParams(lp);
 			
+			// Big button listeners
 			newButton.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					Log.i("Log", v.getTag().toString());
+					String btnTag = v.getTag().toString();
+					Log.i("Button", btnTag);
+					modeSelected(btnTag);
 				}
 			});
 			
@@ -140,22 +156,61 @@ public class MainActivity extends Activity {
 			btnContainer.addView(newButton);
 			containerButtons.addView(btnContainer);
 		}
+		
+		// Back button listener
+		btnBack.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				backToTheStart();
+			}
+		});
 	}
 	
+	/**
+	 * Go back to the start screen
+	 */
+	public void backToTheStart(){
+		viewLocation.setVisibility(View.INVISIBLE);
+		viewStart.setVisibility(View.VISIBLE);
+	}
+	
+	/**
+	 * One of the big buttons was clicked
+	 * @param mode
+	 */
+	public void modeSelected(String mode){
+		if(mode.equals("WALK")){
+			lblMode.setText(getString(R.string.btn_walk));
+		}else if(mode.equals("BICYCLE")){
+			lblMode.setText(getString(R.string.btn_bike));
+		}else if(mode.equals("TRANSIT")){
+			lblMode.setText(getString(R.string.btn_public_transport));
+		}else if(mode.equals("CAR")){
+			lblMode.setText(getString(R.string.btn_car));
+		}
+	
+		viewStart.setVisibility(View.INVISIBLE);
+		viewLocation.setVisibility(View.VISIBLE);
+	}
+	
+	/**
+	 * Prevent leaving the application without password
+	 */
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_POWER:
 		case KeyEvent.KEYCODE_HOME:
 		case KeyEvent.KEYCODE_BACK:
-			// Prevents leaving the application without password
+			// Create an alert to prompt password
 			AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
 			alert.setTitle("A password is needed to quit the application");
 			alert.setMessage("Password:");
 
 			final EditText input = new EditText(this);
 			alert.setView(input);
+			
+			// Add cancel and ok button
 			alert.setPositiveButton("Ok",
 					new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog,
@@ -174,7 +229,8 @@ public class MainActivity extends Activity {
 					// Canceled.
 				}
 			});
-			// Force keyboard
+			
+			// Show the alert
 			alert.show();
 			return true;
 		default:
